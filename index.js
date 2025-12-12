bbbe091c47e1e177f5469b045d26e7e8ecaced21
Himanshu Mehta <himanshujio103@gmail.com>
2025-12-12 18:27:19 +0530

    last commit

---
+++ index.js
@@ -0,0 +1,38 @@
+import cookieParser from "cookie-parser";
+import cors from "cors";
+import dotenv from "dotenv";
+import express from "express";
+import applicationRoute from "./routs/application.route.js";
+import companyRoute from "./routs/company.route.js";
+import jobRoute from "./routs/job.route.js";
+import userRoute from "./routs/user.route.js";
+import connectDB from "./utils/db.js";
+dotenv.config({});
+
+const app = express();
+
+
+
+//middleware
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+app.use(cookieParser());
+const corsOptions={
+    origin:'http://localhost:5173',
+    credentials:true
+}
+app.use(cors(corsOptions));
+
+const PORT=process.env.PORT || 3000;
+
+// api's
+app.use("/api/v1/user",userRoute);
+app.use("/api/v1/company",companyRoute);
+app.use("/api/v1/job",jobRoute);
+app.use("/api/v1/application",applicationRoute);
+
+
+app.listen(PORT,()=>{
+    connectDB();
+    console.log(`Server running at port ${PORT}`);
+})
