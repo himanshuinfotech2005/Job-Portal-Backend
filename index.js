2cbac75e749851a0efeebf632bf845fd868cc9d1
Himanshu Mehta <himanshujio103@gmail.com>
2025-12-12 19:02:27 +0530

    last commi

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 app.use(cookieParser());
 const corsOptions={
-    origin:'https://job-portal-omega-black.vercel.app/',
+    origin:'https://job-portal-omega-black.vercel.app',
     credentials:true
 }
 app.use(cors(corsOptions));
